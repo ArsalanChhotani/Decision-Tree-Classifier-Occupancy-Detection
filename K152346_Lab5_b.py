2097766f00b55393da9cbd2e3b771a0f351d859b
Arsalan Chhotani <53829072+ArsalanChhotani@users.noreply.github.com>
2020-06-09 13:35:55 +0500

    Add files via upload

---
+++ K152346_Lab5_b.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 10:08:21 2019
+
+@author: Arsalan Ashraf
+"""
+
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import cross_validate
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.tree import DecisionTreeClassifier
+
+
+#types = {"Temperature" :float,"Humidity" :float,"Light" :float,"CO2" :float,"HumidityRatio" :float,"Occupancy" :int}
+names = ["Temperature","Humidity","Light","CO2","HumidityRatio","Occupancy"]
+dataset = pd.read_csv("E:\\Laptop Stuff 2020\\DS\\XYZ\\Data Science Assignments\\Labs\\Lab_5\\Occupency_Detection\\datatraining.txt", usecols=names) #dtypes = types #
+
+
+#array = dataset.values
+X_train = dataset.iloc[:,2:5]
+
+Y_train = dataset["Occupancy"]
+print(X_train)
+dataset = pd.read_csv("E:\\Laptop Stuff 2020\\DS\\XYZ\\Data Science Assignments\\Labs\\Lab_5\\Occupency_Detection\\datatest.txt",usecols=names)
+dataset.append(pd.read_csv("E:\\Laptop Stuff 2020\\DS\\XYZ\\Data Science Assignments\\Labs\\Lab_5\\Occupency_Detection\\datatest2.txt",usecols=names))
+
+
+X_test = dataset.iloc[:,2:5]
+Y_test = dataset["Occupancy"]
+
+
+clf = DecisionTreeClassifier()
+clf.fit(X_train, Y_train)
+predictions = clf.predict(X_test)
+print(accuracy_score(Y_test, predictions))
+print(confusion_matrix(Y_test, predictions))
+print(classification_report(Y_test, predictions))
